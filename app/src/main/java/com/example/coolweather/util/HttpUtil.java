7ab738f1dffc42043f39a94dd8ca443fa75c90ba
张学宁 <429301963@qq.com>
2017-03-16 20:39:22 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/example/coolweather/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.example.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by zxn on 2017-3-16.
+ * 现在我们发起一条HTTP请求只需要调用sendOkHttpRequest()方法
+ * 传入请求地址，并注册一个回调来处理服务器响应就可以了
+ */
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
