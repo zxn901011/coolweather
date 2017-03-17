df04854f534097238bea171a260bded93c7f316d
张学宁 <429301963@qq.com>
2017-03-17 11:04:05 +0800

    布局文件的添加

--- app/src/main/java/com/example/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/example/coolweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.coolweather;
+package com.example.coolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
